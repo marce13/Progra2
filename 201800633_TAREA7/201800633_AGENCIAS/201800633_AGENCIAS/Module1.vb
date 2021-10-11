a63c50090e016d5e28203ba17eea1c42f84cbdeb
marce13 <88915895+marce13@users.noreply.github.com>
2021-10-10 21:48:59 -0600

    Add files via upload

---
+++ 201800633_TAREA7/201800633_AGENCIAS/201800633_AGENCIAS/Module1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+    Public FILA As Byte = 0
+    Public i As Byte = 0
+    Public J As Byte = 0
+    Public MONTO(7) As Integer
+    Public EDAD(7) As String
+    Public CODIGO(7) As String
+    Public TALLER(7) As String
+    Public GENERO(7) As String
+
+    Sub inicializar_datos()
+        Form1.txtedad.Clear()
+        Form1.CBcodigo.Text = ""
+        Form1.CBgenero.Text = ""
+        Form1.CBtaller.Text = ""
+        Form1.txtmonto.Clear()
+        Form1.txtnombre.Clear()
+        Form1.txtnombre.Focus()
+    End Sub
+
+    Sub LIMPIAR_VECTORES()
+        FILA = 0
+        Form1.DGVresultados.Rows.Clear()
+        Form1.txtedad.Text = ""
+        Form1.txtmonto.Text = ""
+        Form1.txtnombre.Text = ""
+        Form1.CBcodigo.Text = ""
+        Form1.CBgenero.Text = ""
+        Form1.CBtaller.Text = ""
+
+    End Sub
+End Module
